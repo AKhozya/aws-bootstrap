c8bae5d7ecc23c3502660e0d81309723b3c6b91c
AKhozya <alexander.khozya@gmail.com>
2025-03-17 17:53:15 +0000

    even simpler fix

--- stop-service.sh
+++ stop-service.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/app/release
+[ -d "/home/ec2-user/app/release" ] && \
+cd /home/ec2-user/app/release && \
 npm stop || true
-
-# Always succeed regardless of what happened above
 exit 0
